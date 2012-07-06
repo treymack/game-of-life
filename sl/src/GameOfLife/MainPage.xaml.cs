2ed31ba79270f0a1511f1beed78ffd85b2d7e84c
Trey Mack <tmack10@gmail.com>
2012-07-06 02:24:04 -0400

    Setup basic SL project structure.

---
+++ sl/src/GameOfLife/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using GameOfLife.Model;
+
+namespace GameOfLife
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var g = new Game();
+            for (var r = 0; r < g.NumberOfRows; r++)
+            {
+                this.LayoutRoot.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+    }
+}
---
+++ sl/src/GameOfLife/Model/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife.Model
+{
+    public class Game
+    {
+        public int NumberOfRows { get; set; }
+        public int NumberOfCols { get; set; }
+
+        public List<List<Cell>> Board { get; set; }
+    }
+}
